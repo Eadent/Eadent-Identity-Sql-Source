bc5474f532fd09e3593ba7e8f2cb278cb6394362
Eamonn Duffy <Eamonn@Duffy.global>
2021-05-02 10:40:06 +0000

    feat: Latest Iteration of Eadent Identity Sql for Sql Server.
    https://www.Eadent.com/

--- Sql/00. EadentIdentity.sql
+++ Sql/00. EadentIdentity.sql
@@ -27,12 +27,46 @@
 -- Create Tables if/as appropriate.
 --------------------------------------------------------------------------------
 
+IF OBJECT_ID(N'$(Schema).UserStatuses', N'U') IS NULL
+BEGIN
+	CREATE TABLE $(Schema).UserStatuses
+	(
+		UserStatusId				SmallInt NOT NULL CONSTRAINT PK_$(Schema)_UserStatusess PRIMARY KEY,
+		Description					NVarChar(128) NOT NULL,
+		CreatedDateTimeUtc          DateTime2(7) NOT NULL CONSTRAINT DF_$(Schema)_UserStatuses_CreatedDateTimeUtc DEFAULT GetUtcDate()
+	);
+
+	INSERT INTO $(Schema).UserStatuses
+		(UserStatusId, Description)
+	VALUES
+		(0, N'Enabled'),
+		(1, N'Disabled'),
+		(2, N'Sign In Locked Out'),
+		(100, N'Soft Deleted');
+END
+GO
+
+DECLARE @Error AS Int = @@ERROR;
+IF (@Error != 0)
+BEGIN
+	IF @@TRANCOUNT > 0
+		ROLLBACK TRANSACTION;
+	BEGIN TRANSACTION;
+	SET CONTEXT_INFO 0x01;
+END
+
 IF OBJECT_ID(N'$(Schema).Users', N'U') IS NULL
 BEGIN
 	CREATE TABLE $(Schema).Users
 	(
 		UserId				        BigInt NOT NULL CONSTRAINT PK_$(Schema)_Users PRIMARY KEY IDENTITY(0, 1),
+		UserStatusId				SmallInt NOT NULL CONSTRAINT FK_$(Schema)_Users_UserStatuses FOREIGN KEY (UserStatusId) REFERENCES $(Schema).UserStatuses(UserStatusId),
 		DisplayName					NVarChar(256) NOT NULL,
+		Password					NVarChar(256) NOT NULL,
+		Salt						NVarChar(256) NOT NULL,
+		SignInErrorCount			Int NOT NULL,
+		SignInErrorLimit			Int NOT NULL,
+		SignInLockOutDateTimeUtc	DateTime2(7) NULL,
 		CreatedDateTimeUtc          DateTime2(7) NOT NULL,
 		LastUpdatedDateTimeUtc		DateTime2(7) NULL
 	);
