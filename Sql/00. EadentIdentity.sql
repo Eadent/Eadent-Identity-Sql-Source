c42a6c46e687d48a1eafbc036db57cb162aa4758
Eamonn Duffy <Eamonn@Duffy.global>
2021-05-02 11:17:41 +0000

    feat: Latest Iteration of Eadent Identity Sql for Sql Server.
    https://www.Eadent.com/

--- Sql/00. EadentIdentity.sql
+++ Sql/00. EadentIdentity.sql
@@ -2,18 +2,18 @@
 -- Copyright © 2021+ Éamonn Anthony Duffy. All Rights Reserved.
 --------------------------------------------------------------------------------
 --
--- Created:	Éamonn A. Duffy, 2-May-2021.
+-- Created: Éamonn A. Duffy, 2-May-2021.
 --
--- Purpose:	Main Sql File for the Eadent IDentity Sql Server Database.
+-- Purpose: Main Sql File for the Eadent Identity Sql Server Database.
 --
 -- Assumptions:
 --
---	0.	The Sql Server Database has already been Created by some other means,
---		and has been selected for Use.
+--  0.  The Sql Server Database has already been Created by some other means,
+--      and has been selected for Use.
 --
---	1.	This Sql file will be included in another Sql Project along the lines of:
+--  1.  This Sql file will be included in another Sql Project along the lines of:
 --
---			:R "\Projects\Eadent\Eadent-Identity-Sql-Source\Sql\00. EadentIdentity.sql"
+--          :R "\Projects\Eadent\Eadent-Identity-Sql-Source\Sql\00. EadentIdentity.sql"
 --
 --------------------------------------------------------------------------------
 
@@ -21,7 +21,7 @@
 -- Some Variables.
 --------------------------------------------------------------------------------
 
-:SETVAR Schema							"Dad"
+:SETVAR Schema                          "Dad"
 
 --------------------------------------------------------------------------------
 -- Create Tables if/as appropriate.
@@ -29,81 +29,137 @@
 
 IF OBJECT_ID(N'$(Schema).UserStatuses', N'U') IS NULL
 BEGIN
-	CREATE TABLE $(Schema).UserStatuses
-	(
-		UserStatusId				SmallInt NOT NULL CONSTRAINT PK_$(Schema)_UserStatusess PRIMARY KEY,
-		Description					NVarChar(128) NOT NULL,
-		CreatedDateTimeUtc          DateTime2(7) NOT NULL CONSTRAINT DF_$(Schema)_UserStatuses_CreatedDateTimeUtc DEFAULT GetUtcDate()
-	);
+    CREATE TABLE $(Schema).UserStatuses
+    (
+        UserStatusId                SmallInt NOT NULL CONSTRAINT PK_$(Schema)_UserStatusess PRIMARY KEY,
+        Description                 NVarChar(128) NOT NULL,
+        CreatedDateTimeUtc          DateTime2(7) NOT NULL CONSTRAINT DF_$(Schema)_UserStatuses_CreatedDateTimeUtc DEFAULT GetUtcDate()
+    );
 
-	INSERT INTO $(Schema).UserStatuses
-		(UserStatusId, Description)
-	VALUES
-		(0, N'Enabled'),
-		(1, N'Disabled'),
-		(2, N'Sign In Locked Out'),
-		(100, N'Soft Deleted');
+    INSERT INTO $(Schema).UserStatuses
+        (UserStatusId, Description)
+    VALUES
+        (  0, N'Enabled'),
+        (  1, N'Disabled'),
+        (  2, N'Sign In Locked Out'),
+        (100, N'Soft Deleted');
 END
 GO
 
 DECLARE @Error AS Int = @@ERROR;
 IF (@Error != 0)
 BEGIN
-	IF @@TRANCOUNT > 0
-		ROLLBACK TRANSACTION;
-	BEGIN TRANSACTION;
-	SET CONTEXT_INFO 0x01;
+    IF @@TRANCOUNT > 0
+        ROLLBACK TRANSACTION;
+    BEGIN TRANSACTION;
+    SET CONTEXT_INFO 0x01;
 END
+
+--------------------------------------------------------------------------------
 
 IF OBJECT_ID(N'$(Schema).Users', N'U') IS NULL
 BEGIN
-	CREATE TABLE $(Schema).Users
-	(
-		UserId				        BigInt NOT NULL CONSTRAINT PK_$(Schema)_Users PRIMARY KEY IDENTITY(0, 1),
-		UserStatusId				SmallInt NOT NULL CONSTRAINT FK_$(Schema)_Users_UserStatuses FOREIGN KEY (UserStatusId) REFERENCES $(Schema).UserStatuses(UserStatusId),
-		DisplayName					NVarChar(256) NOT NULL,
-		Password					NVarChar(256) NOT NULL,
-		Salt						NVarChar(256) NOT NULL,
-		SignInErrorCount			Int NOT NULL,
-		SignInErrorLimit			Int NOT NULL,
-		SignInLockOutDateTimeUtc	DateTime2(7) NULL,
-		CreatedDateTimeUtc          DateTime2(7) NOT NULL,
-		LastUpdatedDateTimeUtc		DateTime2(7) NULL
-	);
+    CREATE TABLE $(Schema).Users
+    (
+        UserId                      BigInt NOT NULL CONSTRAINT PK_$(Schema)_Users PRIMARY KEY IDENTITY(0, 1),
+        UserStatusId                SmallInt NOT NULL CONSTRAINT FK_$(Schema)_Users_UserStatuses FOREIGN KEY (UserStatusId) REFERENCES $(Schema).UserStatuses(UserStatusId),
+        DisplayName                 NVarChar(256) NOT NULL,
+        Password                    NVarChar(256) NOT NULL,
+        Salt                        NVarChar(256) NOT NULL,
+        SignInErrorCount            Int NOT NULL,
+        SignInErrorLimit            Int NOT NULL,
+        SignInLockOutDateTimeUtc    DateTime2(7) NULL,
+        CreatedDateTimeUtc          DateTime2(7) NOT NULL,
+        LastUpdatedDateTimeUtc      DateTime2(7) NULL
+    );
 END
 GO
 
 DECLARE @Error AS Int = @@ERROR;
 IF (@Error != 0)
 BEGIN
-	IF @@TRANCOUNT > 0
-		ROLLBACK TRANSACTION;
-	BEGIN TRANSACTION;
-	SET CONTEXT_INFO 0x01;
+    IF @@TRANCOUNT > 0
+        ROLLBACK TRANSACTION;
+    BEGIN TRANSACTION;
+    SET CONTEXT_INFO 0x01;
 END
+
+--------------------------------------------------------------------------------
 
 IF OBJECT_ID(N'$(Schema).UserEMails', N'U') IS NULL
 BEGIN
-	CREATE TABLE $(Schema).UserEMails
-	(
-		UserEMailId				    BigInt NOT NULL CONSTRAINT PK_$(Schema)_UserEMails PRIMARY KEY IDENTITY(0, 1),
-		UserId						BigInt NOT NULL CONSTRAINT FK_$(Schema)_UserEMails_Users FOREIGN KEY (UserId) REFERENCES $(Schema).Users(UserId),
-		EMailAddress				NVarChar(256) NOT NULL,
-		CreatedDateTimeUtc          DateTime2(7) NOT NULL,
-		LastUpdatedDateTimeUtc		DateTime2(7) NULL,
+    CREATE TABLE $(Schema).UserEMails
+    (
+        UserEMailId                 BigInt NOT NULL CONSTRAINT PK_$(Schema)_UserEMails PRIMARY KEY IDENTITY(0, 1),
+        UserId                      BigInt NOT NULL CONSTRAINT FK_$(Schema)_UserEMails_Users FOREIGN KEY (UserId) REFERENCES $(Schema).Users(UserId),
+        EMailAddress                NVarChar(256) NOT NULL,
+        CreatedDateTimeUtc          DateTime2(7) NOT NULL,
+        LastUpdatedDateTimeUtc      DateTime2(7) NULL,
 
-		CONSTRAINT UQ_$(Schema)_UserEMails_UserId_EMailAddress UNIQUE (UserId, EMailAddress) 
-	);
+        CONSTRAINT UQ_$(Schema)_UserEMails_UserId_EMailAddress UNIQUE (UserId, EMailAddress) 
+    );
 END
 GO
 
 DECLARE @Error AS Int = @@ERROR;
 IF (@Error != 0)
 BEGIN
-	IF @@TRANCOUNT > 0
-		ROLLBACK TRANSACTION;
-	BEGIN TRANSACTION;
-	SET CONTEXT_INFO 0x01;
+    IF @@TRANCOUNT > 0
+        ROLLBACK TRANSACTION;
+    BEGIN TRANSACTION;
+    SET CONTEXT_INFO 0x01;
+END
+
+--------------------------------------------------------------------------------
+
+IF OBJECT_ID(N'$(Schema).Roles', N'U') IS NULL
+BEGIN
+    CREATE TABLE $(Schema).Roles
+    (
+        RoleId                      SmallInt NOT NULL CONSTRAINT PK_$(Schema)_Roles PRIMARY KEY,
+        RoleLevel                   SmallInt NOT NULL,
+        Description                 NVarChar(128) NOT NULL,
+        CreatedDateTimeUtc          DateTime2(7) NOT NULL CONSTRAINT DF_$(Schema)_Roles_CreatedDateTimeUtc DEFAULT GetUtcDate()
+    );
+
+    INSERT INTO $(Schema).Roles
+        (RoleId, RoleLevel, Description)
+    VALUES
+        (0,  1000, N'Global Administrator'),
+        (1, 30000, N'Member');
+END
+GO
+
+DECLARE @Error AS Int = @@ERROR;
+IF (@Error != 0)
+BEGIN
+    IF @@TRANCOUNT > 0
+        ROLLBACK TRANSACTION;
+    BEGIN TRANSACTION;
+    SET CONTEXT_INFO 0x01;
+END
+
+--------------------------------------------------------------------------------
+
+IF OBJECT_ID(N'$(Schema).UserRoles', N'U') IS NULL
+BEGIN
+    CREATE TABLE $(Schema).UserRoles
+    (
+        UserRoleId                  BigInt NOT NULL CONSTRAINT PK_$(Schema)_UserRoles PRIMARY KEY IDENTITY(0, 1),
+        UserId                      BigInt NOT NULL CONSTRAINT FK_$(Schema)_UserRoles_Users FOREIGN KEY (UserId) REFERENCES $(Schema).Users(UserId),
+        RoleId                      SmallInt NOT NULL CONSTRAINT FK_$(Schema)_UserRoles_Roles FOREIGN KEY (RoleId) REFERENCES $(Schema).Roles(RoleId),
+        CreatedDateTimeUtc          DateTime2(7) NOT NULL CONSTRAINT DF_$(Schema)_UserRoles_CreatedDateTimeUtc DEFAULT GetUtcDate()
+    );
+END
+GO
+
+DECLARE @Error AS Int = @@ERROR;
+IF (@Error != 0)
+BEGIN
+    IF @@TRANCOUNT > 0
+        ROLLBACK TRANSACTION;
+    BEGIN TRANSACTION;
+    SET CONTEXT_INFO 0x01;
 END
 
 --------------------------------------------------------------------------------
